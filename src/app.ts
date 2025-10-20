20d860ec6f200bd99b5396ff38bb217564eed2f8
Yuri <yuque7@gmail.com>
2025-10-19 22:08:26 -0300

    Estrutura Inicial do Projeto

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+import cors from 'cors'
+import userRouter from "./routes/userRouter";
+
+export const app = express()
+
+app.use(express.json())
+app.use(cors())
+	
+app.use("/users", userRouter);
+
+app.listen(3003, () => {
+	console.log('Servidor rodando na porta 3003')
+})
