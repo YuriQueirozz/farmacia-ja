905282feb3a15adbfa7888a8ce6e0c54745d3258
Yuri <yuque7@gmail.com>
2025-10-28 20:13:50 -0300

    Adicionando banco de dados Supabase

---
+++ src/database/data.ts
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.supabaseUrl as string;
+const supabaseKey = process.env.supabaseKey as string;
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
