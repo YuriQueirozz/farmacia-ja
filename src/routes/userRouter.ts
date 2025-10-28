ee7dc9305dd29cbfa0aeef658675ae645dbe9567
Yuri <yuque7@gmail.com>
2025-10-28 20:18:42 -0300

    Implementação do Supabase

--- src/routes/userRouter.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getUsers } from "../controller/userController";
-
-const userRouter = Router();
-
-userRouter.get("/usuarios", getUsers);
-
-export default userRouter;
