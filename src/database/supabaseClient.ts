8013f254fa75d36241d682f62ccf21c9c135017b
Daniel <dangosil@yahoo.com.br>
2025-10-30 22:19:32 -0300

    Integrando banco de dados Supabase

--- src/database/supabaseClient.ts
+++ src/database/supabaseClient.ts
@@ -1,6 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.supabaseUrl as string;
-const supabaseKey = process.env.supabaseKey as string;
+const supabaseUrl = process.env.SUPABASE_URL as string;
+const supabaseKey = process.env.SUPABASE_ANON_KEY as string;
+
+if(!supabaseUrl || !supabaseKey) {
+    throw new Error("Variáveis de ambiente supabase não definidas.")
+}
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
