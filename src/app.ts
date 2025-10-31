428c23fa843fd054c5ab31574419b6c59e894b76
Daniel <dangosil@yahoo.com.br>
2025-10-30 22:41:25 -0300

    Configurando servidor

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,7 @@
-import express from 'express'
-import cors from 'cors'
-import userRouter from "./routes/usuariosRoutes";
+import express from "express";
+import cors from "cors";
 
-export const app = express()
+export const app = express();
 
-app.use(express.json())
-app.use(cors())
-	
-app.use("/users", userRouter);
-
-app.listen(3003, () => {
-	console.log('Servidor rodando na porta 3003')
-})
+app.use(express.json());
+app.use(cors());
