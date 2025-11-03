654d0cde65364f80bb21ac2108995a81caed7165
Yuri <yuque7@gmail.com>
2025-11-02 21:12:35 -0300

    Revert commit "Estruturando o tipo da entidade Farmacia"
    This reverts commit ab98c7526482a46899a7f560564d4305577a8514.

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import usuariosRoutes from "./routes/usuariosRoutes";
 import medicamentosRoutes from "./routes/medicamentosRoutes";
-import farmaciasRoutes from "./routes/farmaciasRoutes";
 
 export const app = express();
 
@@ -11,9 +10,5 @@ app.use(cors());
 
 app.use("/usuarios", usuariosRoutes);
 app.use("/medicamentos", medicamentosRoutes);
-<<<<<<< HEAD
-app.use("/farmacias", farmaciasRoutes);
-=======
 
 export default app;
->>>>>>> 42d8a717c20b05aef9d4e4c400ea1120ebd324c7
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,24 +1,24 @@
 export type Usuario = {
-  id: string;
-  nome: string;
-  cpf: string;
-  endereco: string | null;
-  data_nascimento: string | null;
-  tipo: string;
-  criadoEm?: string | null;
-  atualizadoEm?: string | null;
+    id: string;
+    nome: string;
+    cpf: string;
+    endereco: string | null;
+    data_nascimento: string | null;
+    tipo: string;
+    criadoEm?: string | null;
+    atualizadoEm?: string | null;
 };
 
 export type Endereco = {
-  id: number;
-  logradouro: string;
-  numero?: string;
-  complemento?: string;
-  bairro: string;
-  cidade: string;
-  estado: string;
-  cep: string;
-  localizacao?: any; // vai ser usado mais para frente com o supabase
+    id: number;
+    logradouro: string;
+    numero?: string;
+    complemento?: string;
+    bairro: string;
+    cidade: string;
+    estado: string;
+    cep: string;
+    localizacao?: any; // vai ser usado mais para frente com o supabase
 };
 
 export type Farmacia = {
@@ -27,35 +27,26 @@ export type Farmacia = {
   telefone: string;
   entregas: boolean;
   cnpj: string;
-<<<<<<< HEAD
   // horarioFuncionamento: string;
   endereco_id: number;
-=======
-  horarioFuncionamento: string;
-  enderecoId: number;
->>>>>>> 42d8a717c20b05aef9d4e4c400ea1120ebd324c7
   ativo?: boolean;
   criadoEm?: string;
   atualizadoEm?: string;
 };
 
 export type Medicamento = {
-  id: number;
-  nome: string;
-  principio_ativo: string;
-  dosagem: string;
-  categoria?: string;
-  criadoEm?: string;
-  atualizadoEm?: string;
+    id: number;
+    nome: string;
+    principioAtivo: string;
+    dosagem: string;
+    categoria?: string;
+    criadoEm?: string;
+    atualizadoEm?: string;
 };
 
 export type ApiResponse<T> = {
-  success: boolean;
-  message: string;
-  data?: T;
-  error?: any;
-<<<<<<< HEAD
+    success: boolean;
+    message: string;
+    data?: T;
+    error?: any;
 };
-=======
-};
->>>>>>> 42d8a717c20b05aef9d4e4c400ea1120ebd324c7
