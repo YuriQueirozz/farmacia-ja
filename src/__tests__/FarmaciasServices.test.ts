93652189f0871ff8badfb1ae8816f3a472bc4328
Yuri <yuque7@gmail.com>
2025-11-25 20:30:45 -0300

    Add. Teste E2E para GET ao criar buscar farmácias por bairro para ver como a API lida quando não recebe o resultado esperado.

--- src/__tests__/FarmaciasServices.test.ts
+++ src/__tests__/FarmaciasServices.test.ts
@@ -29,4 +29,33 @@ describe("Farmácias E2E Tests", () => {
       
     });
   });
+
+
+  /**
+   * TESTE 2 - buscar farmácias em bairros sem cadastros
+   * Método - GET /farmacias/bairro/:bairro
+   * Objetivo - verificar como a API lida quando não há resultados
+   */
+  describe("GET /farmacias/bairro/:bairro - Bairro sem farmácias", () => {
+    it("deve retornar array vazio para bairro sem farmácias", async () => {
+      // Provavelmente não existe no banco de dados
+      const response = await request(app).get(
+        "/farmacias/bairro/Bairro Inexistente Teste E2E"
+      );
+      
+      // 1- Deve retornar status 200 (correto) porque mesmo sem resultados
+      expect(response.status).toBe(200);
+
+      // 2- O campo success deve ser true pois a busca funcionou (só não achou nada)
+      expect(response.body.success).toBe(true);
+
+      // 3- Os dados devem ser um array vazio []
+      // toEqual([]) para verificar se é um array vazio
+      expect(response.body.data).toEqual([]);
+      
+      // 4- Verificando se tem a palavra "Não" na resposta
+      expect(response.body.message).toContain("Não");
+
+    });
+  });
 });
