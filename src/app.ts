263c2f1f2b3cef22cce833b1634e4869a00fb407
Daniel <dangosil@yahoo.com.br>
2026-05-26 17:10:15 -0300

    feat: configure strict cors options for security

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,20 @@ import usuariosRoutes from "./routes/usuariosRoutes";
 import medicamentosRoutes from "./routes/medicamentosRoutes";
 import farmaciasRoutes from "./routes/farmaciasRoutes";
 
-
 export const app = express();
 
+const corsOptions = {
+    origin: [
+        "http://localhost:3000",
+        "http://localhost:5173",
+        "https://seu-futuro-site.com",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
-app.use(cors());
 
 app.use("/usuarios", usuariosRoutes);
 app.use("/medicamentos", medicamentosRoutes);
